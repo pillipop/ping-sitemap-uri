ee77130b48611d704798c777f43ad29dd06ccdbe
pillipop <pilipop1@hill-rom.com>
2023-08-02 17:01:56 +0200

    done

---
+++ lib/sitemap_check.ex
@@ -0,0 +1,64 @@
+defmodule SitemapCheck do
+  @moduledoc """
+  Documentation for `SitemapCheck`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SitemapCheck.hello()
+      :world
+
+  """
+  def read_and_parse_xml(path) when is_binary(path) do
+    %{body: content} = HTTPoison.get!(path)
+
+    case remove_xml_declaration(content) do
+      {:ok, [data]} ->
+        data
+        |> unpack_xml()
+        |> Enum.map(&enum_data/1)
+        |> Enum.map(&ping/1)
+        |> IO.inspect([limit: :infinity, pretty: true])
+
+      {:error, reason} ->
+        IO.puts("Error removing XML declaration: #{reason}")
+    end
+  end
+
+  def remove_xml_declaration(content) do
+    case String.split(content, "\n", parts: 2) do
+      [_h | data] ->
+        {:ok, data}
+      _ ->
+        {:error, "XML declaration not found"}
+    end
+  end
+
+  defp ping(url) do
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200}} ->
+        {:ok, {200, url}}
+      {:ok, %HTTPoison.Response{status_code: 301, body: body}} ->
+        {:ok, {301, url, body}}
+      {:ok, %HTTPoison.Response{status_code: 404, body: body}} ->
+        {:ok, {404, url, body}}
+      {:ok, %HTTPoison.Response{status_code: 500}} ->
+        {:ok, {500, url}}
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, {reason}}
+    end
+  end
+
+  defp enum_data(data) do
+    {_, [], [uri]} = data
+    uri
+  end
+
+  defp unpack_xml(data) do
+    Floki.parse_document!(data)
+    |> Floki.find("loc")
+  end
+end
---
+++ test/sitemap_check_test.exs
@@ -0,0 +1,8 @@
+defmodule SitemapCheckTest do
+  use ExUnit.Case
+  doctest SitemapCheck
+
+  test "greets the world" do
+    assert SitemapCheck.hello() == :world
+  end
+end
